43d914a3febb039f824a77c0a0049c03a366aba5
Bananek96 <Banasiak_Jakub@wp.pl>
2023-03-24 15:37:18 +0100

    Simple website, jupyter notebook into python - How to make API?

--- website/main/views.py
+++ website/main/views.py
@@ -1,5 +1,16 @@
+import requests
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
 
 def index(request):
     return HttpResponse("Hello world!")
+
+
+def generate_speech(request):
+    text_input = request.GET.get('text')
+    url = 'http://localhost:8888/tree'  # Replace with your Jupyter Notebook URL
+    payload = {'text': text_input}
+    response = requests.get(url, params=payload)
+    audio = response.content
+    return render(request, 'main/index.html', {'audio': audio})
