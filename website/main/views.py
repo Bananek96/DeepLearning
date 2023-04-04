6f0afd5eba0499c92561a414b7b7894baf162d42
Bananek96 <Banasiak_Jakub@wp.pl>
2023-04-04 16:21:55 +0200

    Update

--- website/main/views.py
+++ website/main/views.py
@@ -1,15 +1,23 @@
 import requests
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import torch
 
 
-def index(request):
-    return HttpResponse("Hello world!")
+
+class Tacotron2API(APIView):
+    def post(self, request):
+        text = request.data.get('text')
+        model_path = '/../../tacotron2/model.py'
+        synthesizer = Synthesizer(model_path)
+        speech = synthesizer.synthesize(text)
+        return Response({'speech': speech})
 
 
 def generate_speech(request):
     text_input = request.GET.get('text')
-    url = 'http://localhost:8888/tree'  # Replace with your Jupyter Notebook URL
+    url = 'http://localhost:8888/tree/interference.py'  # Replace with your Jupyter Notebook URL
     payload = {'text': text_input}
     response = requests.get(url, params=payload)
     audio = response.content
