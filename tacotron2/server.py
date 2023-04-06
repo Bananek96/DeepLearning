9b1cd9c86049c41dabd611545993083d0991374c
Bananek96 <Banasiak_Jakub@wp.pl>
2023-04-06 13:26:28 +0200

    server update

--- tacotron2/server.py
+++ tacotron2/server.py
@@ -11,6 +11,28 @@ import sys
 
 sys.path.append('waveglow/')
 
+hparams = create_hparams()
+hparams.sampling_rate = 22050
+
+checkpoint_path = "D:/github/For_Project/tacotron2_statedict.pt"
+# checkpoint_path = "C:\Users\piter\Desktop\GitHub\ForProject\tacotron2_statedict.pt"
+# checkpoint_path = "C:\Users\huber\Desktop\0STUDIA\Projekt_Neuronówa\For_Project\tacotron2_statedict.pt"
+
+model = load_model(hparams)
+model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
+_ = model.cuda().eval().half()
+
+waveglow_path = 'D:/github/For_Project/waveglow_256channels.pt'
+# waveglow_path = 'C:\Users\piter\Desktop\GitHub\ForProject\waveglow_256channels.pt'
+# waveglow_path = 'C:\Users\huber\Desktop\0STUDIA\Projekt_Neuronówa\For_Project\waveglow_256channels.pt'
+
+waveglow = torch.load(waveglow_path)['model']
+waveglow.cuda().eval().half()
+for k in waveglow.convinv:
+    k.float()
+denoiser = Denoiser(waveglow)
+
+
 app = Flask(__name__)
 
 
@@ -21,37 +43,18 @@ def plot_data(data, figsize=(16, 4)):
                        interpolation='none')
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def generate_speech():
-    hparams = create_hparams()
-    hparams.sampling_rate = 22050
 
-    checkpoint_path = "D:/github/For_Project/tacotron2_statedict.pt"
-    # checkpoint_path = "C:\Users\piter\Desktop\GitHub\ForProject\tacotron2_statedict.pt"
-    # checkpoint_path = "C:\Users\huber\Desktop\0STUDIA\Projekt_Neuronówa\For_Project\tacotron2_statedict.pt"
-
-    model = load_model(hparams)
-    model.load_state_dict(torch.load(checkpoint_path)['state_dict'])
-    _ = model.cuda().eval().half()
-
-    waveglow_path = 'D:/github/For_Project/waveglow_256channels.pt'
-    # waveglow_path = 'C:\Users\piter\Desktop\GitHub\ForProject\waveglow_256channels.pt'
-    # waveglow_path = 'C:\Users\huber\Desktop\0STUDIA\Projekt_Neuronówa\For_Project\waveglow_256channels.pt'
-
-    waveglow = torch.load(waveglow_path)['model']
-    waveglow.cuda().eval().half()
-    for k in waveglow.convinv:
-        k.float()
-    denoiser = Denoiser(waveglow)
     text_input = request.args.get('text')
     sequence = np.array(text_to_sequence(text_input, ['english_cleaners']))[None, :]
     sequence = torch.autograd.Variable(
         torch.from_numpy(sequence)).cuda().long()
 
     mel_outputs, mel_outputs_postnet, _, alignments = model.inference(sequence)
-    plot_data((mel_outputs.float().data.cpu().numpy()[0],
-               mel_outputs_postnet.float().data.cpu().numpy()[0],
-               alignments.float().data.cpu().numpy()[0].T))
+    # plot_data((mel_outputs.float().data.cpu().numpy()[0],
+    #            mel_outputs_postnet.float().data.cpu().numpy()[0],
+    #            alignments.float().data.cpu().numpy()[0].T))
 
     with torch.no_grad():
         audio = waveglow.infer(mel_outputs_postnet, sigma=0.666)
