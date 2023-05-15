5e207550097f8c339b1e288991a76ddfd71c3c3b
Bananek96 <Banasiak_Jakub@wp.pl>
2023-05-15 15:45:41 +0200

    Simple Frontend

--- website/main/views.py
+++ website/main/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.views import View
-from pydub import AudioSegment
-import io
 import requests
 
 
